8e7892289782a0fa189fca22ab18ba9023b7282c
MFAC <ninafroes@gmail.com>
2020-02-03 10:32:29 +0100

    feat: Add middleware

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import { Provider } from 'react-redux'
 import './index.css'
 import App from './components/App'
 import reducer from './reducers'
+// It imports the export default inside of index.js file in middleware folder
+import middleware from './middleware'
 
-const store = createStore(reducer)
+const store = createStore(reducer, middleware)
 
 ReactDOM.render(
   <Provider store={store}>
