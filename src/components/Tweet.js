398ed769539bfd7b97ba14f5b966f71b6b54f0ca
MFAC <ninafroes@gmail.com>
2020-02-03 12:04:14 +0100

    feat: Check valid tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,7 +5,12 @@ import { formatTweet } from '../utils/helpers'
 
 class Tweet extends Component {
   render() {
-    console.log(this.props)
+    const { tweet } = this.props
+
+    if (tweet === null) {
+      return <p>This tweet doesn't exist</p>
+    }
+
     return (
       <div className="tweet">
 
@@ -18,10 +23,13 @@ class Tweet extends Component {
 // Because of this, the mapStateToProps function's second argument (ownProps) will be an object that has an id property with this value.
 function mapStateToProps({ authedUser, users, tweets }, { id }) {
   const tweet = tweets[id]
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null
 
   return {
     authedUser,
-    tweet: formatTweet(tweet, users[tweet.author], authedUser)
+    tweet: tweet 
+      ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+      : null
   }
 }
 
