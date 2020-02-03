4aa1f7304f9c48ff165df6f04218f7250073b5b4
MFAC <ninafroes@gmail.com>
2020-02-03 12:39:37 +0100

    feat: Add loadingBarReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import authedUser from './authedUser'
 import users from './users'
 import tweets from './tweets'
+import { loadingBarReducer } from 'react-redux-loading'
 
 // Combine all reducers into one main, root reducer, which will combine the results of calling the tweets reducer, users reducer, and authedUser reducer into a single state object. 
 // createStore function only accepts a single reducer.
 export default combineReducers({
   authedUser,
   users,
-  tweets
+  tweets,
+  loadingBar: loadingBarReducer
 })
