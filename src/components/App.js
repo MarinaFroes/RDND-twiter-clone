ac4af35e62d206ad3e69b5f281804ad269b735af
MFAC <ninafroes@gmail.com>
2020-02-03 17:53:52 +0100

    fix: Adjust path to TweetPage

--- src/components/App.js
+++ src/components/App.js
@@ -24,7 +24,7 @@ class App extends Component {
             {this.props.loading === true ? null : (
               <div>
                 <Route path="/" exact component={Dashboard} />
-                <Route path="/tweet:id" component={TweetPage} />
+                <Route path="/tweet/:id" component={TweetPage} />
                 <Route path="/new" exact component={NewTweet} />
               </div>
             )}
